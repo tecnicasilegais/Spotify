1ffa5ee36ac047d9d56d344a074b020ad6363806
ITWhiteRaccoon <eduardo.a@edu.pucrs.br>
2020-06-17 22:15:46 -0300

    fixed subgenres, changed progressbar

--- Program.cs
+++ Program.cs
@@ -22,14 +22,15 @@ namespace Spotify
         {
             var lines = File.ReadAllLines(filePath);
 
-            Console.WriteLine("Índice do artista:");
+            Console.Write("Coluna do artista(zero-based):");
             int artistIndex = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Pesquisando gênero musical dos artistas");
 
-            using (var progress = new ProgressBar(lines.Length, "pesquisando gênero musical dos artistas"))
+            using (var progress = new ProgressBar(lines.Length, "", new ProgressBarOptions { ProgressCharacter = '─' }))
             {
                 for (int i = 0; i < lines.Length; i++)
                 {
-                    progress.Tick($"Artist {i + 1} de {lines.Length}");
+                    progress.Tick($"Artista {i + 1} de {lines.Length}");
                     var data = lines[i].Split(',');
                     string appendix = lines[i][^1] == ',' ? "" : ",";
 
@@ -41,24 +42,29 @@ namespace Spotify
                     if (search.Artists.Items.Count <= 0 ||
                         search.Artists.Items[0].Genres.Count <= 0)
                     {
-                        lines[i] += $"{appendix}Desconhecido";
+                        lines[i] += $"{appendix}desconhecido";
                         continue;
                     }
 
                     var genres = search.Artists.Items[0].Genres;
-                    int genreIndex = genres.FindIndex(IsSearchedGenre);
-                    string genre = genreIndex >= 0 ? genres[genreIndex] : genres[0];
+                    string genre = "";
+                    foreach (string s in genres)
+                    {
+                        genre = s switch
+                        {
+                            var a when a.Contains("emo") => "emo",
+                            var a when a.Contains("jazz") => "jazz",
+                            var a when a.Contains("lo-fi") => "lo-fi",
+                            _ => genres[0]
+                        };
+                    }
+
                     lines[i] += $"{appendix}{genre}";
                 }
             }
             Console.WriteLine("Gravando arquivo...");
             File.WriteAllLines(filePath, lines);
             Console.WriteLine("Arquivo salvo com sucesso.");
-        }
-
-        private static bool IsSearchedGenre(string str)
-        {
-            return str.Contains("emo") || str.Contains("lo-fi") || str.Contains("jazz");
         }
     }
 }
